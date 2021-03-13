331f41ba3c2ee9af8611b4d84dadf2da6033eeb3
tzuchienkao <oyan114@gmail.com>
2021-03-13 13:54:09 +0800

    feat: 建立文字主題風格、google font 引入練習、檔案分割練習

---
+++ lib/Theme/config.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_practice/Theme/textTheme.dart';
+
+TextTheme textTheme() {
+  return customTextTheme;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ui_practice/Theme/config.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,20 +12,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: textTheme(),
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -75,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget.title, style: TextStyle(fontSize: textTheme().headline5.fontSize)),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
